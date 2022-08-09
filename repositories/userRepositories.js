ff1d23edd3d9b77af34cc43b5ad9ba9d5677a2ce
barbararech <rech.brb@gmail.com>
2022-08-09 17:59:56 -0300

    fix: fix queries

--- repositories/userRepositories.js
+++ repositories/userRepositories.js
@@ -2,12 +2,12 @@ import db from "../database/db.js";
 
 async function getUserByEmail(email) {
   console.log(email);
-  return db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+  return db.query(`SELECT * FROM "public.users" WHERE email = $1;`, [email]);
 }
 
 async function addUser(email, passwordEncrypted, username, pictureUrl) {
   return db.query(
-    `INSERT INTO users (email, password, username, "pictureUrl") VALUES ($1, $2, $3, $4)`,
+    `INSERT INTO "public.users" (email, password, username, "pictureUrl") VALUES ($1, $2, $3, $4)`,
     [email, passwordEncrypted, username, pictureUrl]
   );
 }
