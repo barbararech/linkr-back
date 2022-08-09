807d65b8e608311d75aa61ba221911cda3f7e78e
barbararech <rech.brb@gmail.com>
2022-08-09 20:26:27 -0300

    feat: add trending hashtags endpoint

---
+++ controllers/hashtagController.js
@@ -0,0 +1,20 @@
+import { hashtagRepository } from "../repositories/hashtagRepositories.js";
+import { userRepository } from "../repositories/userRepositories.js";
+
+export async function trending(req, res) {
+  const { id } = res.locals;
+    console.log(id)
+  try {
+    const { rows: user } = await userRepository.getUserById(id);
+
+    if (user.length === 0) {
+      return res.sendStatus(403);
+    }
+
+    const { rows: trending } = await hashtagRepository.getTrending();
+
+    return res.status(200).send(trending);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+}
--- repositories/userRepositories.js
+++ repositories/userRepositories.js
@@ -5,6 +5,11 @@ async function getUserByEmail(email) {
   return db.query(`SELECT * FROM "users" WHERE email = $1;`, [email]);
 }
 
+async function getUserById(id) {
+  console.log(id);
+  return db.query(`SELECT * FROM "users" WHERE id = $1;`, [id]);
+}
+
 async function addUser(email, passwordEncrypted, username, pictureUrl) {
   return db.query(
     `INSERT INTO "users" (email, password, username, "pictureUrl") VALUES ($1, $2, $3, $4)`,
@@ -14,5 +19,6 @@ async function addUser(email, passwordEncrypted, username, pictureUrl) {
 
 export const userRepository = {
   getUserByEmail,
+  getUserById,
   addUser,
 };
